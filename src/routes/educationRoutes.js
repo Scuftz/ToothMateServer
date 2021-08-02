3222925a1c427566cb84c590942b16ab511f7292
rangatron <aleop213@gmail.com>
2021-08-02 22:28:56 +1200

    finished the eduaction backend

--- src/routes/educationRoutes.js
+++ src/routes/educationRoutes.js
@@ -5,23 +5,20 @@ const Education = mongoose.model("Education");
 const router = express.Router();
 
 router.get("/Education", (req, res) => {
-    try {
-        content = Education.find()
-        res.send(content)
-    } catch (err) {
-        return res.status(422).send({ error: "Could not fetch education content" })
-    }
+    Education.find()
+    .then(education => res.json(education))
+    .catch(err => res.status(404).json({ error: 'No topics found' }));
 });
 
 router.post("/addEducation", async (req, res) => {
-    const { title, content } = req.body; //req.body contains the user sign up details
+    const { topic, content } = req.body; //req.body contains the user sign up details
 
     try {
-        const education = new Education({ topic: title, content: content }); //creating instance of user
-require("./models/Education");
-await education.save(); //saves the user - async operation to save user to DB
+        const education = new Education({ topic, content }); //creating instance of user
+        await education.save(); //saves the user - async operation to save user to DB
+        res.send("")
     } catch (err) {
-       console.log("no")
+       return res.status(422).send({ error: "Could not fetch education content" })
     }
 })
 
