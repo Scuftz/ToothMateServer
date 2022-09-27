4b52b2157c89e0b88dec6ba7f336b3a9b5301f07
Jona Stevenson <jonatutorials@gmail.com>
2022-09-27 20:09:15 +1300

    Add Cors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-require("./models/User");
-require("./models/Education");
-require("./models/Clinic");
-require("./models/Appointment");
-require("./models/ImgModel");
-require("./models/PdfModel");
-const express = require("express");
-const mongoose = require("mongoose");
-const authRoutes = require("./routes/authRoutes");
-const educationRoutes = require("./routes/educationRoutes");
-const clinicRoutes = require("./routes/clinicRoutes");
-const appointmentRoutes = require("./routes/appointmentRoutes");
-const dentalChartRoutes = require("./routes/dentalChartRoutes");
+require('./models/User');
+require('./models/Education');
+require('./models/Clinic');
+require('./models/Appointment');
+require('./models/ImgModel');
+require('./models/PdfModel');
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const educationRoutes = require('./routes/educationRoutes');
+const clinicRoutes = require('./routes/clinicRoutes');
+const appointmentRoutes = require('./routes/appointmentRoutes');
+const dentalChartRoutes = require('./routes/dentalChartRoutes');
 
-const requireAuth = require("./middlewares/requireAuth");
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(authRoutes);
 app.use(educationRoutes);
@@ -22,32 +24,31 @@ app.use(clinicRoutes);
 app.use(appointmentRoutes);
 app.use(dentalChartRoutes);
 
-
 //MongoDB connection
 const mongoUri =
-  "mongodb+srv://tmAdmin:tmAdminR&D@toothmatecluster.82nq5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  'mongodb+srv://tmAdmin:tmAdminR&D@toothmatecluster.82nq5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
 //When the mongoose connection is successful, the following callback function is called
-mongoose.connection.on("connected", () => {
-  console.log("Connected to mongo instance");
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
 });
 
 //When the mongoose connection fails, the following callback function is called with an error object given
-mongoose.connection.on("error", (err) => {
-  console.error("Error connecting to mongo", err);
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
 });
 
 //Root route of application
 //requireAuth is ran first, verifying the JWT, before running the user's request
-app.get("/", requireAuth, (req, res) => {
+app.get('/', requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email}`);
 });
 
 //Make app listen on local port
 app.listen(3000, () => {
-  console.log("Listening on Port 3000");
+  console.log('Listening on Port 3000');
 });
